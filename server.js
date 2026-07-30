05a64cd34bb5f6bf6fa9800712f5138fa327c83c
Anas <anasroyal385@gmail.com>
2026-07-30 21:32:35 +0530

    setup the express.js with app.js and server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('Hello, World!');
+import app from './src/app.js';
+import envConfig from './src/config/env.config.js';
+const port = envConfig.port;
+
+const startServer = () => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+startServer();
---
+++ src/config/env.config.js
@@ -0,0 +1,5 @@
+const _config = {
+  port: process.env.PORT || 3000,
+};
+
+export default Object.freeze(_config);
